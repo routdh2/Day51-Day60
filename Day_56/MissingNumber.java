a5aaac766ab6812718190e63ad17b39e0ce2842b
Dhananjay Rout <routvoice.1993@gmail.com>
2020-06-12 11:30:43 +0530

    Missing number program in java

---
+++ Day_56/MissingNumber.java
@@ -0,0 +1,10 @@
+//Problem Statement: https://leetcode.com/problems/missing-number/
+class Solution {
+    public int missingNumber(int[] nums) {
+        int missing=nums.length;
+        for(int i=0;i<nums.length;i++) {
+            missing=missing^(i^nums[i]);
+        }
+        return missing;
+    }
+}
